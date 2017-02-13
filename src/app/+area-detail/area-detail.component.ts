320272dfad038ad9e154f97b3b7cc3f0b980ba99
Stefan Nieuwenhuis <Stefan.Nieuwenhuis@ordina.nl>
2017-02-13 10:40:25 +0100

    Area detail populated

--- src/app/+area-detail/area-detail.component.ts
+++ src/app/+area-detail/area-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { DataService } from '../shared/data.service';
+import { Area } from '../shared/area';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-area-detail',
@@ -7,11 +10,18 @@ import { DataService } from '../shared/data.service';
   styleUrls: ['./area-detail.component.css']
 })
 export class AreaDetailComponent implements OnInit {
+  area: Area;
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-
+    this.route.params
+      .switchMap((params: Params) => this.dataService.getTownById(+params['id']))
+      .subscribe((area: Area) => this.area = area[0].attributes);
   }
 
 }
--- src/app/+area-detail/area-detail.module.ts
+++ src/app/+area-detail/area-detail.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 import { AreaDetailComponent }   from './area-detail.component';
 import { AreaDetailRoutingModule} from './area-detail-routing.module';
 
 @NgModule({
-    imports: [AreaDetailRoutingModule],
+    imports: [CommonModule, AreaDetailRoutingModule],
     exports: [],
     declarations: [AreaDetailComponent],
     providers: [],
